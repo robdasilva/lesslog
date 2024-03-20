04cabe7916e96702f7271ca276365908db2488ec
Robert Da Silva <mail@robdasilva.com>
2024-03-20 13:12:51 +0100

    chore: swap husky and lint-staged for lefthook

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "**/*!(.(d|spec|test)).{js,jsx,ts,tsx}": [
-    "jest --bail --findRelatedTests",
-    "eslint --fix",
-    "prettier --write",
-  ],
-  "**/*.(d|spec|test).{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
-  "**/*.{md,json}": ["prettier --write"],
-};
